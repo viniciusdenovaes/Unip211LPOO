71f573deea5a69e6e7882ff9b9e851c376d314e7
vinicius de novaes <viniciusdenovaes@gmail.com>
2021-02-09 20:52:49 -0300

    primeiro exemplo

---
+++ Aula00IDE/src/Ola.java
@@ -0,0 +1,9 @@
+
+public class Ola {
+	
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo");
+	}
+	
+
+}
---
+++ aula00/Ola.java
@@ -0,0 +1,7 @@
+class Ola{
+
+    public static void main(String[] args){
+        System.out.println("Ola Mundo");
+    }
+}
+
