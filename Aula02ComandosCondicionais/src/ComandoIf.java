eb06894b8fa2bd17773aaa369a297dd2c1a5d876
vinicius de novaes <viniciusdenovaes@gmail.com>
2021-02-09 22:02:06 -0300

    aula de comandos condicionais

---
+++ Aula02ComandosCondicionais/src/ComandoIf.java
@@ -0,0 +1,31 @@
+
+public class ComandoIf {
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 3;
+		
+		if(a<b) {
+			System.out.println("a eh menor que b");
+		}
+		
+		if(b<a) {
+			System.out.println("b eh menor que a");
+		}
+		
+		int c = 5;
+		
+		if(a <= c) {
+			System.out.println("a eh menor ou igual a c");
+		}
+		
+		if(a >= c) {
+			System.out.println("a eh maior ou igual a c");
+		}
+		
+		
+		
+	}
+
+}
---
+++ Aula02ComandosCondicionais/src/ComandoIfElse.java
@@ -0,0 +1,31 @@
+
+public class ComandoIfElse {
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 3;
+		int c = 2;
+		
+		if(a<b) {
+			System.out.println("a eh menor que b");
+		} else {
+			System.out.println("a eh maior ou igual a b");
+		}
+		
+		System.out.println("\nVerificacao com if else if:\n");
+		
+		if(a<b) {
+			System.out.println("a eh menor que b");
+		} else if(a>b) {
+			System.out.println("a eh maior que b");
+		} else if (a==b) {
+			System.out.println("a eh igual a b");
+		}
+		
+		
+		
+		
+	}
+
+}
